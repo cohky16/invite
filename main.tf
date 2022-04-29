be339c3307df9fa7b54d1e10d3b0a3cf1ebaf7dd
koki yasuda <45580421+cohky16@users.noreply.github.com>
2022-04-29 22:32:22 +0900

    feat: #41 terraform導入 (#42)
    * feat: #41 terraform導入
    
    terraform導入
    
    closes #41
    
    * feat: #41 terraform動作確認
    
    terraform動作確認
    
    #41
    
    * feat: #41 terraformをgithubactionsで実行
    
    terraformをgithubactionsで実行
    
    closes #41
    
    * fix: #41 スクリプト修正
    
    #41
    
    * fix: #41 github-script6 -> v6
    
    * fix: #41 ワークフロー統一
    
    * fix: #41 needs追加
    
    #41
    
    * fix: #41 tf fmt
    
    #41
    
    * fix: #41 workspeces追加
    
    closes #41
    
    * fix: #41 planのログ削除
    
    closes #41
    
    * fix: #41 push処理追加
    
    closes #41

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    heroku = {
+      source = "heroku/heroku"
+    }
+  }
+
+  cloud {
+    organization = "cohky"
+
+    workspaces {
+      name = "invite"
+    }
+  }
+}
+
+
+provider "heroku" {}
+
+variable "APP_ENV" {}
+variable "DISCORD_TOKEN" {}
+
+resource "heroku_app" "invite-ydkk" {
+  acm = false
+  buildpacks = [
+    "heroku/go"
+  ]
+  config_vars = {
+    APP_ENV : var.APP_ENV
+    DISCORD_TOKEN : var.DISCORD_TOKEN
+  }
+  internal_routing      = false
+  name                  = "invite-ydkk"
+  region                = "us"
+  sensitive_config_vars = {}
+  space                 = null
+  stack                 = "heroku-20"
+}
+
+resource "heroku_build" "invite-ydkk" {
+  app_id = heroku_app.invite-ydkk.id
+  source {
+    path = "."
+  }
+}
+
+resource "heroku_formation" "worker" {
+  app_id     = heroku_app.invite-ydkk.id
+  quantity   = 1
+  size       = "Free"
+  type       = "worker"
+  depends_on = [heroku_build.invite-ydkk]
+}
